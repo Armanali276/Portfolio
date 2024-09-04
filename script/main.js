4f883c59bb31f6bbe561c9df314a56899c94eb3d
Armaan Ali <armanbhatti276@gmail.com>
2024-09-04 13:43:33 +0500

    updated

--- script/main.js
+++ script/main.js
@@ -1,15 +1,7 @@
-var sticky = document.querySelector('.sticky-wrapper');
-window.addEventListener('scroll', function() {
-    let scrollLength = window.pageYOffset;
-    if (scrollLength > 0) {
-        sticky.classList.add('sticky')
-        console.log(scrollLength, document.documentElement.scrollHeight)
-    }else {
-        sticky.classList.remove('sticky')
-    }
-    if(scrollLength > 100) {
-        sticky.classList.add('shrink')
-    }else {
-        sticky.classList.remove('shrink')
-    }
-})
+const sticky = document.querySelector('.sticky-wrapper');
+
+window.addEventListener('scroll', () => {
+    const scrollY = window.pageYOffset;
+    sticky.classList.toggle('sticky', scrollY > 0);
+    sticky.classList.toggle('shrink', scrollY > 100);
+});
