b4adb1a665bb4584ec97145835726c3f343426e6
Armaan Ali <armanbhatti276@gmail.com>
2024-09-04 18:46:39 +0500

    update

--- script/main.js
+++ script/main.js
@@ -1,7 +1,8 @@
 const sticky = document.querySelector('.sticky-wrapper');
-
 window.addEventListener('scroll', () => {
     const scrollY = window.pageYOffset;
     sticky.classList.toggle('sticky', scrollY > 0);
     sticky.classList.toggle('shrink', scrollY > 100);
 });
+
+AOS.init();
